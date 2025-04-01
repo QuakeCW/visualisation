6f51d016cf049b99306776b8da261071356e9d6e
Jake Faulkner <jakefaulkn@gmail.com>
2025-04-01 16:11:49 +1300

    feat: add domain inspection tools

---
+++ visualisation/realisations/plot_domain.py
@@ -0,0 +1,248 @@
+from pathlib import Path
+from typing import Annotated
+
+import numpy as np
+import pandas as pd
+import pygmt
+import shapely
+import typer
+from velocity_modelling.bounding_box import BoundingBox
+
+from pygmt_helper import plotting
+from qcore import cli, coordinates
+from qcore.uncertainties import mag_scaling
+from visualisation.realisations import plot_domain
+from workflow.realisations import (
+    DomainParameters,
+    RupturePropagationConfig,
+    SourceConfig,
+    VelocityModelParameters,
+)
+from workflow.scripts import generate_velocity_model_parameters
+
+app = typer.Typer()
+
+Region = tuple[float, float, float, float]
+
+
+def plot_domain(
+    realisation_ffp: Path,
+    region: Region,
+    width: float,
+    latitude_pad: float,
+    longitude_pad: float,
+    title: str | None = None,
+    subtitle: str | None = None,
+) -> tuple[pygmt.Figure, Region]:
+    domain_parameters = DomainParameters.read_from_realisation(realisation_ffp)
+    domain = polygon_nztm_to_pygmt(domain_parameters.domain.polygon)
+
+    plot_polygon(fig, domain, pen="1p,blue,-")
+    return fig, region
+
+
+def plot_stations(fig: pygmt.Figure, domain: BoundingBox, stations_path: Path) -> None:
+    stations = pd.read_csv(
+        stations_path, delimiter=r"\s+", comment="#", names=["lon", "lat", "name"]
+    )
+    stations_in_domain = np.count_nonzero(
+        domain.contains(stations[["lat", "lon"]].to_numpy())
+    )
+    fig.plot(
+        x=stations["lon"],
+        y=stations["lat"],
+        style="t0.1c",
+        fill="red",
+        pen="black",
+        label=f"Stations ({stations_in_domain})",
+    )
+
+
+def bounding_region_for(
+    polygon: shapely.Polygon | list[shapely.Polygon],
+    latitude_pad: float,
+    longitude_pad: float,
+) -> Region:
+    if isinstance(polygon, list):
+        polygon = shapely.union_all(polygon)
+
+    bounds = shapely.bounds(polygon_nztm_to_pygmt(polygon))
+    return (
+        bounds[0] - longitude_pad,
+        bounds[2] + longitude_pad,
+        bounds[1] - latitude_pad,
+        bounds[3] + latitude_pad,
+    )
+
+
+@cli.from_docstring(app)
+def plot_domain_to_file(
+    realisation_ffp: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to realisation file.", dir_okay=False, exists=True, readable=True
+        ),
+    ],
+    output_ffp: Annotated[
+        Path,
+        typer.Argument(
+            help="Path to output image file path", dir_okay=False, writable=True
+        ),
+    ],
+    latitude_pad: Annotated[
+        float, typer.Option(help="Latitude padding in degrees.", min=0)
+    ] = 0,
+    longitude_pad: Annotated[
+        float,
+        typer.Option(help="Longitude padding in degrees.", min=0),
+    ] = 0,
+    title: Annotated[
+        str | None,
+        typer.Option(
+            help="Title of the plot.",
+        ),
+    ] = None,
+    subtitle: Annotated[
+        str | None,
+        typer.Option(
+            help="Subtitle of the plot.",
+        ),
+    ] = None,
+    width: Annotated[
+        float,
+        typer.Option(
+            help="Width of the plot in cm.",
+            min=0,
+        ),
+    ] = 10,
+    dpi: Annotated[
+        float,
+        typer.Option(
+            help="DPI of the plot (higher is better quality).",
+            min=0,
+        ),
+    ] = 300,
+    show_geonet_stations: Annotated[
+        bool,
+        typer.Option(
+            help="Show GeoNet stations on the plot.",
+            show_default=False,
+        ),
+    ] = False,
+    show_geometry: Annotated[
+        bool,
+        typer.Option(
+            help="Show source geometry on the plot.",
+        ),
+    ] = True,
+    show_pgv_targets: Annotated[
+        bool,
+        typer.Option(
+            help="Show PGV targets on the plot.",
+        ),
+    ] = False,
+    pgv_targets: Annotated[
+        list[float] | None,
+        typer.Option(
+            help="PGV targets to plot. If None, use PGV targets from the realisation.",
+        ),
+    ] = None,
+    stations: Annotated[
+        Path | None,
+        typer.Option(
+            help="Path to stations file to plot.",
+            exists=True,
+            readable=True,
+        ),
+    ] = None,
+) -> None:
+    """Plot the domain of a realisation to a file.
+
+    Parameters
+    ----------
+    realisation_ffp : Path
+            Path to the realisation file to plot.
+    output_ffp : Path
+            Path to output image file path.
+    latitude_pad : float
+            Latitude padding in degrees.
+    longitude_pad : float
+            Longitude padding in degrees.
+    title : str, optional
+            Title of the plot.
+    subtitle : str, optional
+            Subtitle of the plot.
+    width : float
+            Width of the plot in cm.
+    """
+
+    rupture_propagation = RupturePropagationConfig.read_from_realisation(
+        realisation_ffp
+    )
+    domain = DomainParameters.read_from_realisation(realisation_ffp).domain
+
+    velocity_model_parameters = VelocityModelParameters.read_from_realisation(
+        realisation_ffp
+    )
+
+    source_config = SourceConfig.read_from_realisation(realisation_ffp)
+
+    source_geometry = shapely.union_all(
+        [source.geometry for source in source_config.source_geometries.values()]
+    )
+    rrup_bounding_polygons: list[shapely.Polygon] = []
+    region = None
+    if show_pgv_targets:
+        fault_pgv_targets = pgv_targets or [
+            generate_velocity_model_parameters.pgv_target(
+                rupture_propagation, velocity_model_parameters
+            )
+        ]
+
+        for pgv_targets in fault_pgv_targets:
+            rrup_bounding_polygons.append(
+                shapely.union_all(
+                    [
+                        generate_velocity_model_parameters.find_rrup_bounding_polygon(
+                            *args, pgv_target=pgv_targets
+                        )
+                        for args in generate_velocity_model_parameters.dict_zip(
+                            source_config.source_geometries,
+                            rupture_propagation.magnitudes,
+                            rupture_propagation.rakes,
+                        ).values()
+                    ]
+                )
+            )
+
+    region = bounding_region_for(
+        [domain.polygon] + rrup_bounding_polygons,
+        latitude_pad=latitude_pad,
+        longitude_pad=longitude_pad,
+    )
+
+    fig = plotting.gen_region_fig(
+        title,
+        region,
+        projection=f"M{width}c",
+        subtitle=subtitle,
+    )
+
+    plot_polygon(fig, polygon_nztm_to_pygmt(domain.polygon), pen="1p,blue,-")
+
+    if show_geometry:
+        plot_polygon(fig, polygon_nztm_to_pygmt(source_geometry), pen="0.3p,black")
+
+    if show_pgv_targets:
+        for rrup_polygon in rrup_bounding_polygons:
+            plot_polygon(fig, polygon_nztm_to_pygmt(rrup_polygon), pen="0.3p,black,-")
+
+    if stations:
+        plot_stations(fig, domain, stations)
+        fig.legend(position="jTR+o0.2c", box="+gwhite+p1p")
+
+    fig.savefig(output_ffp, dpi=dpi)
+
+
+if __name__ == "__main__":
+    app()
--- visualisation/utils.py
+++ visualisation/utils.py
@@ -3,6 +3,10 @@
 from typing import Optional
 
 import numpy as np
+import pygmt
+import shapely
+
+from qcore import coordinates
 
 
 def format_description(
@@ -41,3 +45,110 @@ def format_description(
     if compact:
         return f"{min_label} / {mean_label} / {std_label} / {max_label}"
     return f"{min_label}\n{mean_label} ({std_label})\n{max_label}"
+
+
+def nztm_to_wgs_wraparound(coords: np.ndarray) -> np.ndarray:
+    """Convert NZTM coordinates to WGS84, wrapping around the international date line for PyGMT.
+
+
+    Parameters
+    ----------
+    coords : np.ndarray
+        NZTM coordinates to convert.
+
+    Returns
+    -------
+    np.ndarray
+        WGS84 coordinates, wrapped around the international date line.
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> coords = np.array([5238700.07489416, 1518491.35216903])
+    >>> nztm_to_wgs_wraparound(coords)
+    array([172.0, -43.0])
+    >>> coords = np.array(coordinates.wgs_depth_to_nztm(np.array([-43, 181])))
+    >>> nztm_to_wgs_wraparound(coords)
+    array([181.0, -43.0])
+    """
+    coords = coordinates.nztm_to_wgs_depth(coords)[:, ::-1]
+    coords[coords[:, 0] < 0, 0] += 360
+    return coords
+
+
+def polygon_nztm_to_pygmt(polygon: shapely.Polygon) -> shapely.Polygon:
+    """Convert a polygon from NZTM to WGS84, wrapping around the international date line for PyGMT.
+
+    Parameters
+    ----------
+    polygon : shapely.Polygon
+        Polygon to convert.
+
+    Returns
+    -------
+    shapely.Polygon
+        Converted polygon.
+
+    Examples
+    --------
+    >>> import shapely
+    >>> p = shapely.Point(5238700.07489416, 1518491.35216903)
+    >>> polygon_nztm_to_pygmt(p)
+    <POINT (172 -43)>
+    >>> q = shapely.Point(*coordinates.wgs_depth_to_nztm(np.array([-43, 181])))
+    >>> polygon_nztm_to_pygmt(q)
+    <POINT (181 -43)>
+    >>> # Note that the coordinates would be negative if coordinates
+    >>> # were not wrapped around the international date line.
+    """
+    return shapely.transform(
+        polygon,
+        lambda x: nztm_to_wgs_wraparound(x),
+    )
+
+
+def plot_polygon(
+    fig: pygmt.Figure,
+    polygon: shapely.LineString
+    | shapely.MultiLineString
+    | shapely.Polygon
+    | shapely.MultiPolygon,
+    **kwargs,
+) -> None:
+    """Plot a polygon on a pygmt figure.
+
+    Parameters
+    ----------
+    fig : pygmt.Figure
+        Figure to plot on.
+    polygon : polygon, linestring, or collection of polygons or linestrings
+        Polygon to plot.
+    **kwargs
+        Additional arguments to pass to `pygmt.Figure.plot`.
+
+    Examples
+    --------
+    >>> import pygmt
+    >>> import shapely.geometry
+    >>> fig = pygmt.Figure()
+    >>> polygon = shapely.geometry.Polygon([(0, 0), (1, 0), (1, 1), (0, 1)])
+    >>> plot_polygon(fig, polygon, pen="1p,blue,-")
+    """
+
+    if isinstance(polygon, shapely.MultiPolygon | shapely.MultiLineString):
+        for part in polygon.geoms:
+            plot_polygon(fig, part, **kwargs)
+    elif isinstance(polygon, shapely.LineString):
+        coords = np.array(polygon.coords)
+        fig.plot(
+            x=coords[:, 0],
+            y=coords[:, 1],
+            **kwargs,
+        )
+    else:
+        polygon_coords = np.array(polygon.exterior.coords)
+        fig.plot(
+            x=polygon_coords[:, 0],
+            y=polygon_coords[:, 1],
+            **kwargs,
+        )
